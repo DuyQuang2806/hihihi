f2e2d6027ee44cf39060e3eff383a7ef659ca8f8
DuyQuang2806 <139487599+DuyQuang2806@users.noreply.github.com>
2023-07-13 23:23:20 +0700

    Add files via upload

---
+++ bai2.js
@@ -0,0 +1,79 @@
+function numberOneTriangle(num) {
+    if(num==1){
+        console.log('*')
+    }
+    else if (num==2){
+        console.log('*')
+        console.log('**')
+    }
+    else if (num==3){
+        console.log('*')
+        console.log('**')
+        console.log('***')
+    }
+    else if (num==4){
+        console.log('*')
+        console.log('**')
+        console.log('***')
+        console.log('****')
+    }
+    else if (num==5){
+        console.log('*')
+        console.log('**')
+        console.log('***')
+        console.log('****')
+        console.log('*****')
+    }
+    else if (num==6){
+        console.log('*')
+        console.log('**')
+        console.log('***')
+        console.log('****')
+        console.log('*****')
+        console.log('******')
+    }
+    else if (num==7){
+        console.log('*')
+        console.log('**')
+        console.log('***')
+        console.log('****')
+        console.log('*****')
+        console.log('******')
+        console.log('*******')
+    }
+    else if (num==8){
+        console.log('*')
+        console.log('**')
+        console.log('***')
+        console.log('****')
+        console.log('*****')
+        console.log('******')
+        console.log('*******')
+        console.log('********')
+    }
+    else if (num==9){
+        console.log('*')
+        console.log('**')
+        console.log('***')
+        console.log('****')
+        console.log('*****')
+        console.log('******')
+        console.log('*******')
+        console.log('********')
+        console.log('*********')
+    }
+    else if (num==10){
+        console.log('*')
+        console.log('**')
+        console.log('***')
+        console.log('****')
+        console.log('*****')
+        console.log('******')
+        console.log('*******')
+        console.log('********')
+        console.log('*********')
+        console.log('**********')
+    }
+}
+
+numberOneTriangle(2)
